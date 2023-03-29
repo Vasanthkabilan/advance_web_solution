c9b2816c114382963c34244d64d61d64a88ea166
Vasanth <68269283+Vasanthkabilan@users.noreply.github.com>
2023-03-29 13:18:57 +0100

    Updated client.php

--- admin/Client.php
+++ admin/Client.php
@@ -2,91 +2,98 @@
 session_start();
 include "db.php";
 include "sidenav.php";
-if(isset($_POST['btn_save']))
-{
-$firstname=$_POST['firstname'];
-$lastname=$_POST['lastname'];
-$email=$_POST['email'];
+if (isset($_POST['btn_save'])) {
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $email = $_POST['email'];
+  $password_hash=$_POST['password_hash'];
 
-mysqli_query($con,"insert into client(firstname, lastname, email, password_hash) values ('$firstname','$lastname','$email')") 
-			or die ("Query 1 is inncorrect........");
-header("location: Client.php"); 
-mysqli_close($con);
+  mysqli_query($con, "insert into client(firstname, lastname, email, password_hash ) values ('$firstname','$lastname','$email','$password_hash')")
+    or die("Query 1 is inncorrect........");
+  header("location: Client.php");
+  mysqli_close($con);
 }
 
 ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-          <!-- your content here -->
-          <div class="col-md-12">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title">Add Employee</h4>
+<!-- End Navbar -->
+<div class="content">
+  <div class="container-fluid">
+    <!-- your content here -->
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">Add Employee</h4>
+        </div>
+        <div class="card-body">
+          <form action="" method="post" name="form" enctype="multipart/form-data">
+            <div class="row">
+
+              <div class="col-md-3">
+                <div class="form-group bmd-form-group">
+                  <label class="bmd-label-floating">First Name</label>
+                  <input type="text" id="firstname" name="firstname" class="form-control" required>
                 </div>
-                <div class="card-body">
-                  <form action="" method="post" name="form" enctype="multipart/form-data">
-                    <div class="row">
-                      
-                      <div class="col-md-3">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">First Name</label>
-                          <input type="text" id="firstname" name="firstname" class="form-control" required>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-group bmd-form-group">
-                          <label class="bmd-label-floating">Last Name</label>
-                          <input type="text" name="lastname" id="lastname"  class="form-control" required>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
+              </div>
+              <div class="col-md-4">
+                <div class="form-group bmd-form-group">
+                  <label class="bmd-label-floating">Last Name</label>
+                  <input type="text" name="lastname" id="lastname" class="form-control" required>
+                </div>
+              </div>
+            </div>
+            <div class="row">
                       <div class="col-md-6">
                         <div class="form-group bmd-form-group">
                           <label class="bmd-label-floating">Email</label>
                           <input type="email" name="email" id="email" class="form-control" required>
                         </div>
                       </div>
+                      <div class="col-md-6">
+                        <div class="form-group bmd-form-group">
+                          <label class="bmd-label-floating">Password</label>
+                          <input type="password" id="password" name="password_hash" class="form-control" required>
+                        </div>
+                      </div>
                     </div>
-                    <button type="submit" name="btn_save" id="btn_save" class="btn btn-primary pull-right">Add Employee</button>
-                    <div class="clearfix"></div>
-                  </form>
-                </div>
-              </div>
-            </div>
+            <button type="submit" name="btn_save" id="btn_save" class="btn btn-primary pull-right">Add Employee</button>
+            <div class="clearfix"></div>
+          </form>
         </div>
       </div>
+    </div>
+  </div>
+</div>
 
-      <div class="content">
-        <div class="container-fluid">
-         <div class="col-md-14">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title">Manage Employees</h4>
-              </div>
-              <div class="card-body">
-                <div class="table-responsive ps">
-                  <table class="table tablesorter table-hover" id="">
-                    <thead class=" text-primary">
-                      
-                      <tr>
-                        <th>firstname</th>
-                        <th>Email</th>
-                      
-	<!-- <th><a href="adduser.php" class="btn btn-success">Add New</a></th> -->
-                    </tr></thead>
-                    <tbody>
-                      <?php 
-                        $result=mysqli_query($con,"select id,firstname, email, password_hash from client")or die ("query 2 incorrect.......");
+<div class="content">
+  <div class="container-fluid">
+    <div class="col-md-14">
+      <div class="card ">
+        <div class="card-header card-header-primary">
+          <h4 class="card-title">Manage Employees</h4>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive ps">
+            <table class="table tablesorter table-hover" id="">
+              <thead class=" text-primary">
 
-                        while(list($user_id,$user_name,$user_email)=
-                        mysqli_fetch_array($result))
-                        {
-                        echo "<tr><td>$user_name</td>";
-                        echo "<td>$user_email</td>";
+                <tr>
+                  <th>firstname</th>
+                  <th>Email</th>
 
-                        echo"<td>
+                  <!-- <th><a href="adduser.php" class="btn btn-success">Add New</a></th> -->
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $result = mysqli_query($con, "select id,firstname, email from client") or die("query 2 incorrect.......");
+
+                while (list($user_id, $user_name, $user_email) =
+                  mysqli_fetch_array($result)
+                ) {
+                  echo "<tr><td>$user_name</td>";
+                  echo "<td>$user_email</td>";
+
+                  echo "<td>
                         <a href='edituser.php?user_id=$user_id' type='button' rel='tooltip' title='' class='btn btn-info btn-link btn-sm' data-original-title='Edit User'>
                                 <i class='material-icons'>edit</i>
                               <div class='ripple-container'></div></a>
@@ -94,15 +101,21 @@ mysqli_close($con);
                                 <i class='material-icons'>close</i>
                               <div class='ripple-container'></div></a>
                         </td></tr>";
-                        }
-                        mysqli_close($con);
-                        ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
+                }
+                mysqli_close($con);
+                ?>
+              </tbody>
+            </table>
+            <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
+              <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+            </div>
+            <div class="ps__rail-y" style="top: 0px; right: 0px;">
+              <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
             </div>
           </div>
-          
         </div>
       </div>
+    </div>
+
+  </div>
+</div>
